04bd61aec1c83ca1e6e69e123006d0272b5b0941
Huynh Tien <huynhqtienvtag@gmail.com>
2021-11-23 23:23:40 +0700

    small method to get library from coordinate

--- libby-loader-core/src/main/java/me/hsgamer/libbyloader/LibbyLoaderAPI.java
+++ libby-loader-core/src/main/java/me/hsgamer/libbyloader/LibbyLoaderAPI.java
@@ -1,6 +1,13 @@
 package me.hsgamer.libbyloader;
 
+import net.byteflux.libby.Library;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class LibbyLoaderAPI {
+    private static final Pattern COORDINATE_PATTERN = Pattern.compile("([^: ]+):([^: ]+)(:([^: ]*)(:([^: ]+))?)?:([^: ]+)");
     private static LibraryManagerWrapper manager;
 
     private LibbyLoaderAPI() {
@@ -13,5 +20,26 @@ public final class LibbyLoaderAPI {
 
     public static void setManager(LibraryManagerWrapper manager) {
         LibbyLoaderAPI.manager = manager;
+    }
+
+    /**
+     * Get the library by the coordinate
+     *
+     * @param coords the coordinate
+     * @return the library
+     * @see <a href="https://github.com/apache/maven-resolver/blob/master/maven-resolver-api/src/main/java/org/eclipse/aether/artifact/DefaultArtifact.java">Default Artifact</a>
+     */
+    public static Library getLibrary(String coords) {
+        Matcher m = COORDINATE_PATTERN.matcher(coords);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Bad artifact coordinates " + coords
+                    + ", expected format is <groupId>:<artifactId>[:<extension>[:<classifier>]]:<version>");
+        }
+        return Library.builder()
+                .groupId(m.group(1))
+                .artifactId(m.group(2))
+                .classifier(Optional.ofNullable(m.group(6)).orElse(""))
+                .version(m.group(7))
+                .build();
     }
 }
