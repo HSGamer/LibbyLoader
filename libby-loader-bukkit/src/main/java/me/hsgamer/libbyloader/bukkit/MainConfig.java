5baed932b27501cbdc4407ca24afc0d626584045
Huynh Tien <huynhqtienvtag@gmail.com>
2021-11-23 13:04:31 +0700

    map to core

--- libby-loader-bukkit/src/main/java/me/hsgamer/libbyloader/bukkit/MainConfig.java
+++ libby-loader-bukkit/src/main/java/me/hsgamer/libbyloader/bukkit/MainConfig.java
@@ -3,13 +3,12 @@ package me.hsgamer.libbyloader.bukkit;
 import me.hsgamer.hscore.bukkit.config.BukkitConfig;
 import me.hsgamer.hscore.common.CollectionUtils;
 import me.hsgamer.hscore.config.*;
+import me.hsgamer.libbyloader.LibrarySettingConstants;
 import net.byteflux.libby.Library;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-
-import static me.hsgamer.libbyloader.LibrarySettingConstants.*;
 
 public class MainConfig extends PathableConfig {
     public static final ConfigPath<List<String>> EXTERNAL_REPOSITORIES = new BaseConfigPath<>(
@@ -30,26 +29,13 @@ public class MainConfig extends PathableConfig {
         public @NotNull List<Library> convert(@NotNull Map<String, Map<String, Object>> rawValue) {
             List<Library> list = new ArrayList<>();
             rawValue.forEach((key, value) -> {
-                if (!value.containsKey(GROUP_ID) || !value.containsKey(ARTIFACT_ID) || !value.containsKey(VERSION)) {
-                    return;
+                try {
+                    Library.Builder builder = LibrarySettingConstants.deserialize(value);
+                    builder.id(key);
+                    list.add(builder.build());
+                } catch (Exception ignored) {
+                    // IGNORED
                 }
-                Library.Builder builder = Library.builder().id(key)
-                        .groupId(String.valueOf(value.get(GROUP_ID)))
-                        .artifactId(String.valueOf(value.get(ARTIFACT_ID)))
-                        .version(String.valueOf(value.get(VERSION)));
-                if (value.containsKey(CLASSIFIER)) {
-                    builder.classifier(String.valueOf(value.get(CLASSIFIER)));
-                }
-                if (value.containsKey(CHECKSUM)) {
-                    builder.checksum(String.valueOf(value.get(CHECKSUM)));
-                }
-
-                boolean isolated = Optional.ofNullable(value.get(ISOLATED))
-                        .map(Object::toString)
-                        .map(Boolean::parseBoolean)
-                        .orElse(false);
-                builder.isolatedLoad(isolated);
-                list.add(builder.build());
             });
             return list;
         }
@@ -58,17 +44,7 @@ public class MainConfig extends PathableConfig {
         public @NotNull Map<String, Map<String, Object>> convertToRaw(@NotNull List<Library> value) {
             Map<String, Map<String, Object>> map = new LinkedHashMap<>();
             value.forEach(library -> {
-                Map<String, Object> options = new LinkedHashMap<>();
-                options.put(GROUP_ID, library.getGroupId());
-                options.put(ARTIFACT_ID, library.getArtifactId());
-                options.put(VERSION, library.getVersion());
-                if (library.hasClassifier()) {
-                    options.put(CLASSIFIER, library.getClassifier());
-                }
-                if (library.hasChecksum()) {
-                    options.put(CHECKSUM, library.getChecksum());
-                }
-                options.put(ISOLATED, library.isIsolatedLoad());
+                Map<String, Object> options = LibrarySettingConstants.serialize(library);
                 map.put(library.getId(), options);
             });
             return map;
--- libby-loader-core/src/main/java/me/hsgamer/libbyloader/LibrarySettingConstants.java
+++ libby-loader-core/src/main/java/me/hsgamer/libbyloader/LibrarySettingConstants.java
@@ -1,5 +1,11 @@
 package me.hsgamer.libbyloader;
 
+import net.byteflux.libby.Library;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public final class LibrarySettingConstants {
     public static final String GROUP_ID = "group-id";
     public static final String ARTIFACT_ID = "artifact-id";
@@ -10,5 +16,42 @@ public final class LibrarySettingConstants {
 
     private LibrarySettingConstants() {
         // EMPTY
+    }
+
+    public static Library.Builder deserialize(Map<String, Object> map) {
+        if (!map.containsKey(GROUP_ID) || !map.containsKey(ARTIFACT_ID) || !map.containsKey(VERSION)) {
+            throw new IllegalArgumentException("Missing required fields");
+        }
+        Library.Builder builder = Library.builder()
+                .groupId(String.valueOf(map.get(GROUP_ID)))
+                .artifactId(String.valueOf(map.get(ARTIFACT_ID)))
+                .version(String.valueOf(map.get(VERSION)));
+        if (map.containsKey(CLASSIFIER)) {
+            builder.classifier(String.valueOf(map.get(CLASSIFIER)));
+        }
+        if (map.containsKey(CHECKSUM)) {
+            builder.checksum(String.valueOf(map.get(CHECKSUM)));
+        }
+        Optional.ofNullable(map.get(ISOLATED))
+                .map(Object::toString)
+                .map(Boolean::parseBoolean)
+                .ifPresent(builder::isolatedLoad);
+
+        return builder;
+    }
+
+    public static Map<String, Object> serialize(Library library) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put(GROUP_ID, library.getGroupId());
+        map.put(ARTIFACT_ID, library.getArtifactId());
+        map.put(VERSION, library.getVersion());
+        if (library.hasClassifier()) {
+            map.put(CLASSIFIER, library.getClassifier());
+        }
+        if (library.hasChecksum()) {
+            map.put(CHECKSUM, library.getChecksum());
+        }
+        map.put(ISOLATED, library.isIsolatedLoad());
+        return map;
     }
 }
